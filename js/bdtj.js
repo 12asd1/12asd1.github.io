0d5bc4805f0fe9f3aa9b79de4e58a99574244d3b
admin <2298750363@qq.com>
2020-08-24 21:04:05 +0800

    Commit Message6

---
+++ js/bdtj.js
@@ -0,0 +1,3 @@
+//baidutongji-love
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F907097ffd2cbbfe2edfd992c8d4f3dda' type='text/javascript'%3E%3C/script%3E"));
--- renxi/love.js
+++ renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-	    ctx.fillText(" [亲爱的宝贝]点击心有惊喜", 23, 10);
+	    ctx.fillText(" 点击心有惊喜", 23, 10);
             ctx.restore();
         },
         clear: function() {
